4e9ba1c3f09de10cca73342d3b4f9d5a27ea0b39
Dzikri Hakim <dzhkim21@gmail.com>
2026-07-06 22:21:11 +0700

    Add files via upload

---
+++ Author.java
@@ -0,0 +1,33 @@
+/*
+Nama    : Dzikri Hakim, NIM  : 2501083010
+Desk    : 
+*/
+package praktikum23042026;
+
+public class Author {
+    private final String nama;
+    private String email;
+    private final char gender;
+    
+    public Author(String nama, String email, char gender){
+        this.nama = nama;
+        this.email = email;
+        this.gender = gender;
+    }
+    
+    public String getNama(){
+        return nama;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email= email;
+    }
+    public char getGender(){
+        return gender;
+    }
+    public String toString() {
+    return "Author[nama=" + nama + ", email=" + email + ", gender=" + gender + "]";
+    }
+}
